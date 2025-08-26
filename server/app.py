39abb8a84d6d648cbe19a489f168036337ca3ae9
Eric Brian Njuguna <brianerick.njuguna@gmail.com>
2025-08-26 18:38:53 +0300

    CRUD operations done on app.py and linked backend API to frontend

--- server/app.py
+++ server/app.py
@@ -14,13 +14,83 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=["GET","POST"])
 def messages():
-    return ''
+    if request.method == "GET":
+        message_objs_list = Message.query.all()
+        message_dict_list = [message_obj.to_dict() for message_obj in message_objs_list]
+        response = make_response(message_dict_list,200)
+        return response
+    elif request.method == "POST":
+        # get request from client
+        request_dict = request.get_json()
+        
+        # Check whether fields are empty
+        for key in request_dict:
+            print(request_dict[key])
+            if not request_dict[key]:
+                error_dict = {
+                    'message': f"{key} is empty"
+                }
+                response = make_response(error_dict, 400)
+                return response
+        
+        new_message_obj = Message(username = request_dict.get("username"), body = request_dict.get("body"))
+        db.session.add(new_message_obj)
+        db.session.commit()
 
-@app.route('/messages/<int:id>')
+        new_message_dict = new_message_obj.to_dict()
+        response = make_response(new_message_dict, 200)
+        return response
+
+@app.route('/messages/<int:id>', methods=["PATCH", "DELETE"])
 def messages_by_id(id):
-    return ''
+    target_message_obj = Message.query.filter(Message.id==id).first()
+
+    if target_message_obj:
+        if request.method == "PATCH":
+            print("Hello World")
+            # Get changes from client request
+            client_changes_dict = request.get_json()
+
+            for key in client_changes_dict:
+                updated_value = client_changes_dict[key]
+                if not updated_value: #Check for empty fields
+                    error_dict = {
+                        'message': f"{key} field is empty"
+                    }
+                    response = make_response(error_dict,400)
+                    return response
+                else:
+                    target_message_obj.body = updated_value
+                    db.session.add(target_message_obj)
+                    db.session.commit()
+
+                    response_dict = target_message_obj.to_dict()
+                    response = make_response(response_dict,200)
+                    return response
+
+        elif request.method == "DELETE":
+            db.session.delete(target_message_obj)
+            db.session.commit()
+
+            message_dict = {
+                'message': f"message {id} deleted"
+            }
+            response = make_response(message_dict,200)
+            return response
+        else:
+            error_dict = {
+                'message': "http method is neither patch nor delete"
+            }
+            response = make_response(error_dict,400)
+            return response
+    else:
+        error_dict = {
+            'message': f"id {id} doesn't exist"
+        }
+        response = make_response(error_dict,400)
+        return response
 
 if __name__ == '__main__':
     app.run(port=5555)
